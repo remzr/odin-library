38f3b5f43542a8e191d472abe307ac729512a9a9
remzr <110097561+remzr@users.noreply.github.com>
2025-10-22 15:13:07 +0200

    Create function to display library content in frontend

--- library.js
+++ library.js
@@ -12,10 +12,26 @@ function addBookToLibrary(author, title, pages) {
     return(myLibrary.push(newBook));  
 }
 
+function displayLibrary() {
+    const tableElement = document.querySelector(".library-display");
+
+    myLibrary.map(item => {
+        tableElement.insertAdjacentHTML("beforeend",`
+        <tr>
+            <td>${item.author}</td>
+            <td>${item.title}</td>
+            <td>${item.pages}</td>
+        </tr>      
+        `);
+    });
+}
+
+
 addBookToLibrary("John Smith", "How to code", 320);
 addBookToLibrary("J.K. Rowling", "Harry Potter 1", 413);
 addBookToLibrary("Robert W. Chambers", "The King in Yellow", 310);
 addBookToLibrary("Paolo Coelho", "Nathan the wise", 230);
 
+console.log(myLibrary);
 
-console.log([myLibrary]);
+displayLibrary();
