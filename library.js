c0267534f6f8d5aaac3ada76cdafcb5567b60831
remzr <110097561+remzr@users.noreply.github.com>
2025-10-22 14:28:19 +0200

    Create object constructor for single books

--- library.js
+++ library.js
@@ -1,1 +1,16 @@
+const myLibrary = [];
 
+function Book(author, title, pages ) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary() {
+    //take Params, creat a book then store it in the array
+}
+
+const book1 = new Book("John Smith", "How to code", 320);
+
+console.log(book1);
