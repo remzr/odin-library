f969f018032488e9a10c77eb76607e14571648ed
remzr <110097561+remzr@users.noreply.github.com>
2025-10-23 21:16:21 +0200

    Fix bug where all existing books where repeatedly added

--- library.js
+++ library.js
@@ -1,48 +1,44 @@
 const myLibrary = [];
 
 // Create a book object
-function Book(author, title, pages ) {
+function Book(author, title, pages, stored) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.stored = stored;
     this.id = crypto.randomUUID();
 };
 
 // Add books to library
-function addBookToLibrary(author, title, pages) {
-    const newBook = new Book(author, title, pages);
+function addBookToLibrary(author, title, pages, stored) {
+    const newBook = new Book(author, title, pages, stored);
     return(myLibrary.push(newBook));
-};
-
-//Clear table
-function clearTable() {
-    document.querySelector(".table-row").remove();
 };
 
 // Show library as table in frontend
 function displayLibrary() {
 
-    clearTable();
-
     const tableElement = document.querySelector(".library-display");
 
-    myLibrary.map(item => {
-        tableElement.insertAdjacentHTML("beforeend",`
-        <tr class="table-row">
-            <td class="table-row">${item.title}</td>
-            <td class="table-row">${item.author}</td>
-            <td class="table-row">${item.pages}</td>
-        </tr>      
-        `);
-    });
-     
+    myLibrary.map(item => {    
+        if (item.stored == false) {    
+            tableElement.insertAdjacentHTML("beforeend",`
+            <tr>
+                <td class="table-row">${item.title}</td>
+                <td class="table-row">${item.author}</td>
+                <td class="table-row">${item.pages}</td>
+            </tr>      
+            `);
+        }
+        item.stored = true;
+    });    
 };
 
 //Event listener to add books
 const form = document.querySelector(".addBookForm");
 
 form.addEventListener("submit", function(e) {
-    //Prevent default behaviour -> sending Data to server
+    //Prevent default behaviour -> not sending data to server
     e.preventDefault();
 
     //Create object with form values
@@ -50,15 +46,6 @@ form.addEventListener("submit", function(e) {
     formData = Object.fromEntries(formData);
 
     //Pass values to create book function
-    addBookToLibrary(formData.bookAuthor, formData.bookTitle, formData.bookPages);
+    addBookToLibrary(formData.bookAuthor, formData.bookTitle, formData.bookPages, false);
     displayLibrary("new");
 });
-
-addBookToLibrary("John Smith", "How to code", 320);
-addBookToLibrary("J.K. Rowling", "Harry Potter 1", 413);
-addBookToLibrary("Robert W. Chambers", "The King in Yellow", 310);
-addBookToLibrary("Paolo Coelho", "Nathan the wise", 230);
-
-console.log(myLibrary);
-
-displayLibrary();
